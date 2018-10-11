134910a8139f96ca02b2d91b3271e2c0fab2e79a
Damian <damian.dugan@coding-academy.fr>
2018-10-11 19:11:55 +0200

    Exercise 4

---
+++ ex_04/ruler.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "rubiks.h"
+
+void	print_tab(int **table)
+{
+  int i = 0;
+  printf("-----------------\n");
+  while ( i < 4 )
+    {
+      printf("| %d | %d | %d | %d |", table[i][0], table[i][1], table[i][2], table[i][3]);
+      printf("\n-----------------\n");
+      i++;
+
+    }
+  printf("\n");
+}
+/*int	main()
+{
+  int
+  
+
+  }*/
