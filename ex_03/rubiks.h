09732caf0100c5d91134cf061763c7a3aafe0f4a
Damian <damian.dugan@coding-academy.fr>
2018-10-11 18:24:30 +0200

    Exercise 3

---
+++ ex_03/rubiks.h
@@ -0,0 +1,12 @@
+#ifndef RUBIKS_H_
+#define RUBIKS_H_
+
+void	print_tab(int **table);
+void	algo_line(int **table, int line);
+void	algo_column(int **table, int column);
+void	algo_square(int **table, int square);
+
+void	algo_line_reverse(int **table, int line);
+void	algo_column_reverse(int **table, int column);
+void	algo_square_reverse(int **table, int square);
+#endif
